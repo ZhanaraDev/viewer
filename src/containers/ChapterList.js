1b458208c619a6cdf50888058a8b17f1f3e9680a
Zhanara1 <lesovazhanara@gmail.com>
2018-05-11 14:56:06 +0600

    Verstka i vsyakoe takoe

--- src/containers/ChapterList.js
+++ src/containers/ChapterList.js
@@ -7,9 +7,9 @@ class ChapterList extends Component{
 	showList(){
 		return this.props.chapters.map(
 				(chapter) => {
-					const classes = this.props.chapter_active.id===chapter.id ?"paragraph-current active" : "paragraph-current";
+					const par_current_classes = this.props.chapter_active.id===chapter.id ?"paragraph-current active" : "paragraph-current";
 					return(
-						<div className={classes} href="#" onClick={() => this.props.select(chapter)} key={chapter.id} >
+						<div className={par_current_classes} href="#" onClick={() => this.props.select(chapter)} key={chapter.id} >
 							<ul className="paragraph-title-wrapper">
 			                    <li>
 			                        <div className="paragraph-title">
