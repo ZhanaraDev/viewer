5e5ff8450f7dbd90999c7e7039a109ca2f987f38
Zhanara1 <lesovazhanara@gmail.com>
2018-06-28 14:35:53 +0600

    configs

--- src/settings/production.js
+++ src/settings/production.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = "https://sell.epigraph.kz:2422/";
-export const REST_API_URL = "https://sell.epigraph.kz/restapp/";
+export const SERVER_URL = "http://sell.epigraph.kz:2422/";
+export const REST_API_URL = "http://sell.epigraph.kz/restapp/";
 
 export const STD_HEADERS = {
     "Accept": "application/json",
